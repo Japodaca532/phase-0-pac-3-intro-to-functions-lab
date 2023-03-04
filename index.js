37bf58b17fa5d665a06f629cf2785d4896da0e0f
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-03 22:53:32 -0700

    creates myfile.md

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }function whisper(string) {
+    // todo
+  }function whisper(string) {
+    return string;
+  }
+  'HELLO'.toLowerCase(); // 'HELLO!'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
