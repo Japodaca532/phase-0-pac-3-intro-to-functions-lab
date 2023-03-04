5be8538bbedf8cb23824b09614f0e62ac7524f35
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-04 00:39:44 -0700

    file1.md

--- index.js
+++ index.js
@@ -24,4 +24,17 @@ function shout(string) {
     console.log(string.toLowerCase()
     );
   }
-  
+  function sayHiToHeadphonedRoommate (string) {
+  var cantAnswer = "I can't hear you!";
+  var yesAnswer = "YES INDEED!";
+  var luvAnswer = "I would love to!";
+  if (string.toLowerCase(string) === string) {
+    return cantAnswer
+  }
+  else if (string.toUpperCase(string) === string) {
+    return yesAnswer;
+  } 
+  else if ("Let's have dinner together!" === string) {
+    return luvAnswer
+  }
+}
